cc1de16e0436e5ea23754ae28923acc73db8afd6
suzisuu <190601121+suzisuu@users.noreply.github.com>
2026-01-18 23:28:05 +0300

    feat: finalize password generation and copy functionality

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 const btnEl = document.querySelector(".btn");
 const inputEl = document.getElementById("input");
+const copyIconEl = document.querySelector(".fa-copy");
+const alertContainerEl = document.querySelector(".alert-container");
 
 btnEl.addEventListener("click", () => {
   createPassword();
+});
+
+copyIconEl.addEventListener("click", () => {
+  copyPassword();
+
+  if (inputEl.value) {
+    alertContainerEl.innerText = inputEl.value + " copied!";
+
+    // Show alert
+    alertContainerEl.classList.remove("active");
+    setTimeout(() => {
+      alertContainerEl.classList.add("active");
+    }, 2000);
+  }
 });
 
 function createPassword() {
@@ -15,4 +31,10 @@ function createPassword() {
     password += chars.substring(randomNum, randomNum + 1);
   }
   inputEl.value = password;
+}
+
+function copyPassword() {
+  inputEl.select();
+  inputEl.setSelectionRange(0, 9999);
+  navigator.clipboard.writeText(inputEl.value);
 }
