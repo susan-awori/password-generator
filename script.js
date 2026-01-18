47fa8fb801f7067dc8d0b7a9a8c281ee5c5e1f70
suzisuu <190601121+suzisuu@users.noreply.github.com>
2026-01-18 12:43:48 +0300

    Feat: Add JS for creating password function

---
+++ script.js
@@ -0,0 +1,18 @@
+const btnEl = document.querySelector(".btn");
+const inputEl = document.getElementById("input");
+
+btnEl.addEventListener("click", () => {
+  createPassword();
+});
+
+function createPassword() {
+  const chars =
+    "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()_+?:{}[]ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const passwordLength = 15;
+  let password = "";
+  for (let index = 0; index < passwordLength; index++) {
+    const randomNum = Math.floor(Math.random() * chars.length);
+    password += chars.substring(randomNum, randomNum + 1);
+  }
+  inputEl.value = password;
+}
